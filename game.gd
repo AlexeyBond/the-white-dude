623e4ab54667bba537053519bafb7132d6288912
A.Bond <alexey.bond.94.55@gmail.com>
2023-04-30 14:11:55 +0600

    lv9, lv10, restart

--- game.gd
+++ game.gd
@@ -1,20 +1,29 @@
 extends CanvasLayer
 
+@export var initial_scene: PackedScene
+
+var current_scene: PackedScene = null
 var next_scene: PackedScene = null
+
+func _ready():
+	do_load_scene(initial_scene)
+
+func do_load_scene(scene: PackedScene):
+	var new_scene = scene.instantiate()
+	add_child(new_scene)
+	move_child(new_scene, 0)
+	current_scene = scene
 
 func do_load_next_scene():
 	if next_scene == null:
 		return
-	
-	var current_scene = get_child(0)
-	
-	remove_child(current_scene)
-	current_scene.call_deferred("free")
 
-	var new_scene = next_scene.instantiate()
-	add_child(new_scene)
-	move_child(new_scene, 0)
+	var current = get_child(0)
 
+	remove_child(current)
+	current.call_deferred("free")
+
+	do_load_scene(next_scene)
 	next_scene = null
 
 func change_level(lvl: PackedScene):
@@ -22,3 +31,13 @@ func change_level(lvl: PackedScene):
 		return
 	next_scene = lvl
 	$AnimationPlayer.play("change_scene")
+
+
+func _input(event):
+	if event.is_action_pressed("game_restart"):
+		change_level(initial_scene)
+	elif event.is_action_pressed("game_restart_level"):
+		change_level(current_scene)
+
+
+
