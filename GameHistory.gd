fbc57d58f27a2e74d474e9092a5b76ca19bc2cf0
A.Bond <alexey.bond.94.55@gmail.com>
2023-05-01 19:57:19 +0600

    leaderboard

---
+++ GameHistory.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name GameHistory
+
+@export var plus_mode_unlocked: bool = false
+
+@export var best_score: float = -1
--- GameMode.gd
+++ GameMode.gd
@@ -1,14 +1,31 @@
 extends Node
 
-var _plus_game_mode_allowed: bool = false
+var history: GameHistory
 var _plus_game_mode: bool = false
 var _play_time: float = 0.0
 
+const history_file_path = "user://game_history.tres"
+
+func load_history():
+	if ResourceLoader.exists(history_file_path):
+		history = load(history_file_path)
+	elif history != null:
+		history = GameHistory.new()
+
+
+func save_history():
+	ResourceSaver.save(history, history_file_path)
+
+
+func _ready():
+	load_history()
+
 func plus_game_mode_allowed() -> bool:
-	return _plus_game_mode_allowed
+	return history.plus_mode_unlocked
 
 func allow_plus_game_mode():
-	_plus_game_mode_allowed = true
+	history.plus_mode_unlocked = true
+	save_history()
 
 func plus_game_mode_enabled() -> bool:
 	return _plus_game_mode
@@ -22,5 +39,13 @@ func track_play_time(time: float):
 func get_total_play_time() -> float:
 	return _play_time
 
+func get_best_score() -> float:
+	return history.best_score
+
 func clear_play_time():
 	_play_time = 0.0
+
+func save_score():
+	if history.best_score <= 0.0 or history.best_score > _play_time:
+		history.best_score = _play_time
+		save_history()
